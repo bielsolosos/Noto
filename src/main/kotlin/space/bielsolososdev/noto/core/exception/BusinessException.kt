82c9600fc6f01d3f1d109b07760bdcaba6adfe53
Gabriel Coutinho <125703161+bielsolosos@users.noreply.github.com>
2026-02-23 16:30:52 -0300

    kotlin + java

--- src/main/kotlin/space/bielsolososdev/noto/core/exception/BusinessException.kt
+++
@@ -1,7 +0,0 @@
-package space.bielsolososdev.noto.core.exception
-
-class BusinessException : RuntimeException {
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-}
-
